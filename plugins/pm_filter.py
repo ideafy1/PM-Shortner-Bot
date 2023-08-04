da3c8bd0779ab43b752fd21136a442062c6035c8
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2023-08-04 06:38:33 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -150,8 +150,7 @@ async def next_page(bot, query):
             ]
         )
         btn.insert(0, [
-            InlineKeyboardButton("Sᴛᴀʀᴛ Bᴏᴛ", url=f"https://telegram.me/{temp.U_NAME}"),
-            InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}")
+            InlineKeyboardButton("🎁 ɢᴇᴛ ᴅɪʀᴇᴄᴛ ᴍᴏᴠɪᴇ 🎁", url=f"https://telegram.me/{temp.U_NAME}")
         ])
     else:
         btn = []
@@ -448,8 +447,7 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
         )
     # if ENABLE_SHORTLINK == True:
     btn.insert(0, [
-        InlineKeyboardButton("Sᴛᴀʀᴛ Bᴏᴛ", url=f"https://telegram.me/{temp.U_NAME}"),
-        InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}")
+        InlineKeyboardButton("🎁 ɢᴇᴛ ᴅɪʀᴇᴄᴛ ᴍᴏᴠɪᴇ 🎁", url=f"https://telegram.me/{temp.U_NAME}")
     ])
     # else:
     #     btn.insert(0, [
@@ -1847,8 +1845,7 @@ async def auto_filter(client, msg, spoll=False):
     #         InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"send_fall#{pre}#{message.chat.id}-{message.id}#{0}")
     #     ])
         btn.insert(0, [
-            InlineKeyboardButton("Sᴛᴀʀᴛ Bᴏᴛ", url=f"https://telegram.me/{temp.U_NAME}"),
-            InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}")
+            InlineKeyboardButton("🎁 ɢᴇᴛ ᴅɪʀᴇᴄᴛ ᴍᴏᴠɪᴇ 🎁", url=f"https://telegram.me/{temp.U_NAME}")
         ])
     # if ENABLE_TUTORIAL == True:
     #     btn.insert(0, [
