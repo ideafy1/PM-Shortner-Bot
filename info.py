c904d44f229af70ceadede1e7537659784cf59d8
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2023-08-18 02:50:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,6 +23,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/7215c7831b4ae88897448.jpg https://telegra.ph/file/34f3bd6b0cda5bb9b0cca.jpg https://telegra.ph/file/724bdd44693500c024ec7.jpg https://telegra.ph/file/1b39388dd4a343cc325bc.jpg')).split()
 AREBHAI = (environ.get('AREBHAI', 'https://graph.org/file/2f3db07a3c1e951a09452.jpg')).split()
+ADPIC = (environ.get('ADPIC', 'https://telegra.ph/file/ab9cf864c46f566cc8dff.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
